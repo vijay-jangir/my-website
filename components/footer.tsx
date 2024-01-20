96267c2d3601e1d3b5fc01d3e5194900014b578e
Vijay Jangir <6284383+vijay-jangir@users.noreply.github.com>
2024-01-21 00:46:19 +0530

    updated menu clicking issue over experience and projects

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} Vijay Jangir. All rights reserved.
+        &copy; {currentYear} Vijay Jangir.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
